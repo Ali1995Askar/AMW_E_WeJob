029892fc9677f76b5c5a3b12600c36696975fd1a
Ali1995Askar <ali1995askar@gmail.com>
2020-11-28 17:57:30 +0200

    final push

--- E_WeJob_Repo/E_WeJob/E_WeJob/settings.py
+++ E_WeJob_Repo/E_WeJob/E_WeJob/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "1gd06&)2g4s^hf9n*8391ab6hk@=&)rl$=5$)vcihx)$6v2v-7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
